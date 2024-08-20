29032c0d06bbdedf7d44288778f148e675f37405
hannah <mjko8903@gmail.com>
2024-08-21 08:50:01 +0900

    feat: 라우터 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./errror-page";
+import Update from "./routes/update";
+import Details from "./routes/details";
+import { Search } from "./routes/search";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Search />,
+      },
+      {
+        path: "update",
+        element: <Update />,
+      },
+      {
+        path: "details/:id",
+        element: <Details />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/routes/details.tsx
@@ -0,0 +1,3 @@
+export default function Details() {
+  return <p>상세 페이지</p>;
+}
